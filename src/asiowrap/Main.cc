18e13171855b044a5c333100e05381ec7517f755
cheukw <zhuorongtan@qq.com>
2017-04-14 15:13:42 +0800

    add asio wrapp

---
+++ src/asiowrap/Main.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "EventLoop.h"
+#include "TcpServer.h"
+#include "TcpConn.h"
+
+void OnConnection(const TCPConnPtr& conn) {
+	if (conn->IsConnected()) {
+		std::cout << conn->GetName() <<" connection accepted.\n";
+	}
+	else if (conn->IsDisconnecting()) {
+		std::cout << conn->GetName() <<" connection disconnecting.\n";
+	}
+}
+
+void OnMessage(const TCPConnPtr& conn, ByteBuffer& buffer) {
+	std::cout << "recv msg " << std::string(buffer.Data(), buffer.Size()) << std::endl;
+	conn->Send(buffer.ReadBegin(), buffer.Size());
+	buffer.ReadBytes(buffer.Size());
+	conn->Close();
+}
+
+int main()
+{
+	EventLoop loop;
+	TCPServer s(&loop, "0.0.0.0", 7788, "EchoServer", 0);
+	s.SetConnectionCallback(OnConnection);
+	s.SetMessageCallback(OnMessage);
+	s.Init();
+	s.Start();
+
+	loop.Run();
+
+	return 0;
+}
+
